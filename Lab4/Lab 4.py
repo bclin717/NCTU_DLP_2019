2c7e26d227b71950286fd9bc8f89916782699eba
ds934118 <ds934118@gmail.com>
2019-04-21 21:18:13 +0800

    Fix Bug

--- Lab4/Lab 4.py
+++ Lab4/Lab 4.py
@@ -7,7 +7,7 @@ def showResult(accu):
     plt.title('RNN BPTT Test Results')
     plt.xlabel('Iteration Number')
     plt.ylabel('Accuracy(%)')
-    plt.xlim(0, iter_num)
+    plt.xlim(0, iterNum)
     plt.ylim(0, 110)
     plt.grid()
 
@@ -24,33 +24,31 @@ def deriv_sigmoid(output):
 
 
 # Hyper Parameters
-binary_dim = 8
+dataDim = 8
 alpha = 0.1
-input_dim = 2
-hidden_dim = 16
-output_dim = 1
-iter_num = 20000
-iter_print_num = 200
+inputDim = 2
+hiddenDim = 16
+outputDim = 1
+iterNum = 20000
+iterPrintNum = 200
 accu = {}
 
-# Setting of generating numbers
-largest_num = int(pow(2, binary_dim) / 2)
-smallest_num = int(-pow(2, binary_dim) / 2)
+largest_num = int(pow(2, dataDim) / 2)
+smallest_num = int(-pow(2, dataDim) / 2)
 binary = np.unpackbits(np.array([range(smallest_num, largest_num)], dtype=np.uint8).T, axis=1)
 num_table = {}
 for i in range(smallest_num, largest_num):
     num_table[i + largest_num] = i
 
-# initialize neural network weights
-w0 = np.random.normal(0, 1, [input_dim, hidden_dim])
-w1 = np.random.normal(0, 1, [hidden_dim, output_dim])
-wh = np.random.normal(0, 2, [hidden_dim, hidden_dim])
+w0 = np.random.normal(0, 1, [inputDim, hiddenDim])
+w1 = np.random.normal(0, 1, [hiddenDim, outputDim])
+wh = np.random.normal(0, 2, [hiddenDim, hiddenDim])
 
 d0 = np.zeros_like(w0)
 d1 = np.zeros_like(w1)
 dh = np.zeros_like(wh)
 
-for i in range(iter_num + 1):
+for i in range(iterNum + 1):
     error = 0
 
     a_int = num_table[np.random.randint(64, 192)]
@@ -64,45 +62,40 @@ for i in range(iter_num + 1):
 
     predict = np.zeros_like(c)
 
-    layer_2_deltas = list()
-    layer_1_values = list()
-    layer_1_values.append(np.zeros(hidden_dim))
+    l2_deltas = list()
+    l1_value = list()
+    l1_value.append(np.zeros(hiddenDim))
 
-    for position in range(binary_dim):
-        X = np.array([[a[binary_dim - position - 1], b[binary_dim - position - 1]]])
-        y = np.array([[c[binary_dim - position - 1]]]).T
+    for position in range(dataDim):
+        X = np.array([[a[dataDim - position - 1], b[dataDim - position - 1]]])
+        y = np.array([[c[dataDim - position - 1]]]).T
 
-        # hidden layer
-        layer_1 = sigmoid(np.dot(X, w0) + np.dot(layer_1_values[-1], wh))
+        l1 = sigmoid(np.dot(X, w0) + np.dot(l1_value[-1], wh))
+        y_hat = sigmoid(np.dot(l1, w1))
 
-        # output layer
-        y_hat = sigmoid(np.dot(layer_1, w1))
+        l2_error = y - y_hat
+        l2_deltas.append((l2_error) * deriv_sigmoid(y_hat))
+        error += np.abs(l2_error[0])
 
-        layer_2_error = y - y_hat
-        layer_2_deltas.append((layer_2_error) * deriv_sigmoid(y_hat))
-        error += np.abs(layer_2_error[0])
+        predict[dataDim - position - 1] = np.round(y_hat[0][0])
 
-        predict[binary_dim - position - 1] = np.round(y_hat[0][0])
+        l1_value.append(np.ndarray.copy(l1))
 
-        layer_1_values.append(np.ndarray.copy(layer_1))
+    future_l1_delta = np.zeros(hiddenDim)
 
-    future_layer_1_delta = np.zeros(hidden_dim)
-
-    for position in range(binary_dim):
+    for position in range(dataDim):
         X = np.array([[a[position], b[position]]])
-        layer_1 = layer_1_values[-position - 1]
-        prev_layer_1 = layer_1_values[-position - 2]
+        l1 = l1_value[-position - 1]
+        prev_l1 = l1_value[-position - 2]
 
-        # error
-        layer_2_delta = layer_2_deltas[-position - 1]
-        layer_1_delta = (future_layer_1_delta.dot(wh.T) + layer_2_delta.dot(w1.T)) * deriv_sigmoid(layer_1)
+        l2_d = l2_deltas[-position - 1]
+        l1_delta = (future_l1_delta.dot(wh.T) + l2_d.dot(w1.T)) * deriv_sigmoid(l1)
 
-        # update
-        d1 += np.atleast_2d(layer_1).T.dot(layer_2_delta)
-        dh += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
-        d0 += X.T.dot(layer_1_delta)
+        d1 += np.atleast_2d(l1).T.dot(l2_d)
+        dh += np.atleast_2d(prev_l1).T.dot(l1_delta)
+        d0 += X.T.dot(l1_delta)
 
-        future_layer_1_delta = layer_1_delta
+        future_l1_delta = l1_delta
 
     w0 += d0 * alpha
     w1 += d1 * alpha
@@ -112,16 +105,16 @@ for i in range(iter_num + 1):
     d1 *= 0
     dh *= 0
 
-    if (i % iter_print_num == 0):
+    if (i % iterPrintNum == 0):
         wrong = 0
         for j in range(0, 8):
             if predict[j] != c[j]:
                 wrong += 1
-        accu[i] = ((1 - (wrong / binary_dim)) * 100)
+        accu[i] = ((1 - (wrong / dataDim)) * 100)
 
         ans = int(np.packbits(predict, axis=-1))
         if ans >= largest_num:
-            ans -= pow(2, binary_dim)
+            ans -= pow(2, dataDim)
 
         print("--------------------------------------\n")
         print("      Iter :" + str(i))
